56710db54f3b082a63181aae5e19784b6b12fdbf
jongha <john0820693@gmail.com>
2018-11-04 16:37:23 +0900

    Implemented two buttons(Room Create & TimeTable) in MainActivity, and created two activities(RoomCreate, TimeTable). RoomCreate activity must be changed as a window-looking activity.

--- app/src/main/java/com/example/jongha/meetingorganizer/MainActivity.java
+++ app/src/main/java/com/example/jongha/meetingorganizer/MainActivity.java
@@ -9,15 +9,33 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText emailEdit, pwdEdit;
-    private Button signInBtn, signUpBtn;
-    private String email, pwd;
+    private Button roomCreateBtn, timeTableBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        roomCreateBtn = findViewById(R.id.room_create_btn);
+        timeTableBtn = findViewById(R.id.timetable_modify_btn);
+
+        roomCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //방 생성 버튼 클릭
+                Intent intent = new Intent(MainActivity.this, RoomCreateActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        timeTableBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TimeTableActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
