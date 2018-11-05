42f5a0b84066f2ac504500c7b88cf017a9e38d5d
jongha <john0820693@gmail.com>
2018-11-05 19:49:01 +0900

    Implemented all elements in RoomCreateActivity.java and finished layout design.

--- app/src/main/java/com/example/jongha/meetingorganizer/RoomCreateActivity.java
+++ app/src/main/java/com/example/jongha/meetingorganizer/RoomCreateActivity.java
@@ -3,13 +3,15 @@ package com.example.jongha.meetingorganizer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class RoomCreateActivity extends Activity {
 
-    private EditText roomNameEdit, roomCodeEdit;
+    private EditText roomNameEdit, roomCodeEdit, hourEdit, minEdit;
+    private Button confirmBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,8 +21,24 @@ public class RoomCreateActivity extends Activity {
 
         roomNameEdit = findViewById(R.id.room_name);
         roomCodeEdit = findViewById(R.id.room_code);
+        hourEdit = findViewById(R.id.estimateHour);
+        minEdit = findViewById(R.id.estimateMin);
+        confirmBtn = findViewById(R.id.confirm_create_btn);
 
 
+        confirmBtn.setOnClickListener(new View.OnClickListener(){
+
+            String roomName, roomCode, hour, min;
+            @Override
+            public void onClick(View v) {
+                roomName = roomNameEdit.getText().toString();
+                roomCode = roomCodeEdit.getText().toString();
+                hour = hourEdit.getText().toString();
+                min = minEdit.getText().toString();
+
+                finish();
+            }
+        });
 
     }
 }
