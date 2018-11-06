f4b37743d4dcc7f9d38a714090580cf584be64da
jongha <john0820693@gmail.com>
2018-11-06 21:58:46 +0900

    Created DisplayScheduleActivity, an activity called when clicked specific activity in list view, and displays information about the schedule. There is a delete button used to delete the activity. Delete function should be implemented.

---
+++ app/src/main/java/com/example/jongha/meetingorganizer/DisplayScheduleActivity.java
@@ -0,0 +1,47 @@
+package com.example.jongha.meetingorganizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DisplayScheduleActivity extends Activity {
+
+    private TextView startHourView, startMinView, endHourView, endMinView, dateOfWeekView;
+    private Button confirmBtn, deleteBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_display_schedule);
+
+        startHourView = findViewById(R.id.startHourView);
+        startMinView = findViewById(R.id.startMinView);
+        endHourView = findViewById(R.id.endHourView);
+        endMinView = findViewById(R.id.endMinView);
+        dateOfWeekView = findViewById(R.id.dateOfWeekView);
+        confirmBtn = findViewById(R.id.scheduleOkBtn);
+        deleteBtn=findViewById(R.id.scheduleDeleteBtn);
+
+
+        //onCreate하면 누른 activity의 이름 등 고유정보를 intent로 받아와서 database에서 해당 정보를 찾아 startHourView 등 Textview 내용을 set하는 부분 작성하기
+
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //delete하는것 구현하기
+            }
+        });
+
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
