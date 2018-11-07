647220f323639b6e9293f35a0804056ffed78c71
jongha <john0820693@gmail.com>
2018-11-07 15:49:46 +0900

    Modified text style in xml files. Also modified layout in activity_new_schedule.xml. Changed many view's width and height to wrap_content from a designated value.

--- app/src/main/java/com/example/jongha/meetingorganizer/DisplayScheduleActivity.java
+++ app/src/main/java/com/example/jongha/meetingorganizer/DisplayScheduleActivity.java
@@ -32,7 +32,7 @@ public class DisplayScheduleActivity extends Activity {
         deleteBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                //delete하는것 구현하기
+                //delete하는 기능 구현하기
             }
         });
 
--- app/src/main/java/com/example/jongha/meetingorganizer/SignInActivity.java
+++ app/src/main/java/com/example/jongha/meetingorganizer/SignInActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.android.gms.signin.SignIn;
 
 public class SignInActivity extends AppCompatActivity {
 
